e74d6bd9345e56ee8fa628bb307d6f7bb42228b3
tnp2004 <iceandiceisice2004@gmail.com>
2025-02-09 14:59:42 +0700

    move file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      c1: "#FEFAF6",
+      c2: "#EADBC8",
+      c3: "#DAC0A3",
+      c4: "#102C57",
+      amber: "#fbbf24"
+    },
+  },
+  plugins: [],
+}
