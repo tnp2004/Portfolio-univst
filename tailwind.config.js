04d72ef73e241d2c2730cab990557fd34d383aae
tnp2004 <iceandiceisice2004@gmail.com>
2025-02-22 14:27:53 +0700

    Ex.5 Validation form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
  /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+ 
  export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./exercises/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {},
     colors: {
+      ...colors,
       c1: "#FEFAF6",
       c2: "#EADBC8",
       c3: "#DAC0A3",
       c4: "#102C57",
-      amber: "#fbbf24"
+      amber: "#fbbf24",
     },
   },
   plugins: [],
